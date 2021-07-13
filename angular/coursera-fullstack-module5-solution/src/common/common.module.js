8ac8aaaa490dc9559d5c874e18f700eaa174bad9
wangxi <wangxi009567@sina.com>
2021-07-13 19:18:33 +0800

    modify

--- angular/coursera-fullstack-module5-solution/src/common/common.module.js
+++ angular/coursera-fullstack-module5-solution/src/common/common.module.js
@@ -2,7 +2,7 @@
 "use strict";
 
 angular.module('common', [])
-.constant('ApiPath', 'https://jordan-item.herokuapp.com/')
+.constant('ApiPath', 'https://jordan-course5.herokuapp.com/')
 .config(config);
 
 config.$inject = ['$httpProvider'];
